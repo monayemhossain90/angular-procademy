d96ab9226f11b426065955e203e376321b7d2c0a
monayemhossain90 <monayemhossain1990@gmail.com>
2023-04-08 23:22:56 +0600

    custom attribute directive t-34

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { setBackgroundDirective } from './customDirective/setBackground.directive';
 
-import { LifestyleHookComponent } from './lifestyle-hook/lifestyle-hook.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    LifestyleHookComponent,
- 
+    setBackgroundDirective 
   ],
   imports: [
     BrowserModule,
